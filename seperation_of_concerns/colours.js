e13fef16c2fe2a030898727201fb34edabd23c8c
Mardgelle Neri <mardgelleneri@gmail.com>
2024-03-15 16:34:52 +0000

    intro to js

---
+++ seperation_of_concerns/colours.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  document.querySelectorAll('button').forEach(function(button) {
+    button.onclick = function() {
+      document.querySelector('#hello').style.color = button.dataset.colour;
+    }
+  });
+});
---
+++ seperation_of_concerns/hello.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  document.querySelector('form').onsubmit = function() {
+    const name = document.querySelector('#name').value;
+    alert(`Hello, ${name}!`)
+  };
+
+});
