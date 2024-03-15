a1d5019bd488d3f0846190dafe1d12dc012940fe
Mardgelle Neri <mardgelleneri@gmail.com>
2024-03-15 16:48:10 +0000

    dropdown select

--- seperation_of_concerns/colours.js
+++ seperation_of_concerns/colours.js
@@ -1,8 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-  document.querySelectorAll('button').forEach(button => {
-    button.onclick = function() {
-      document.querySelector('#hello').style.color = button.dataset.colour;
-    }
-  });
+//   document.querySelectorAll('button').forEach(button => {
+//     button.onclick = function() {
+//       document.querySelector('#hello').style.color = button.dataset.colour;
+//     }
+//   });
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('select').onchange = function() {
+    document.querySelector('#hello').style.color = this.value;
+  }
 });
